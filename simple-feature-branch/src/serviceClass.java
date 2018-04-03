e63d47ac37876727cf07ef2ae253acf7e4a6350a
CristianoRonaldo1995 <naveed.farouq@hotmail.co.uk>
2018-04-03 17:34:05 +0100

    first commit

---
+++ simple-feature-branch/src/serviceClass.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class serviceClass {
+	
+	private HashMap<accountClass, Integer> hmap = new HashMap<accountClass, Integer>();
+	
+	public void adding(accountClass firstName, int accountNumber) {
+		
+		hmap.put(firstName, accountNumber);
+	}
+
+	public void retrieving() {
+		System.out.println(hmap.toString());
+	}
+	
+
+}
